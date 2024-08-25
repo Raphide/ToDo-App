ea1ea5ce48b84f0de15582440e5b893f630b8121
Raphide <atlas.dino.usic@outlook.com>
2024-08-25 22:59:11 +1000

    added sorting and edit

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,4 +1,4 @@
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs';
 import { TodoResponse } from "../../services/todo-services";
 import { Link } from "react-router-dom";
 
@@ -7,14 +7,18 @@ interface TaskCardProps {
   onDelete: (id: number) => Promise<unknown>;
 }
 
+
+
 const TaskCard = ({ task, onDelete }: TaskCardProps) => {
-    
+  
+  const createdAt = dayjs(task.createdAt).locale('au').format('DD-MM-YYYY HH:mm:ss')
+  
   return (
     <div>
       <h2>{task.task}</h2>
       <h3>{task.category}</h3>
       <h5>Priority: {task.priority}</h5>
-      <h6>{task.createdAt}</h6>
+      <h6>{createdAt}</h6>
       <p>{task.description}</p>
       <Link to={`${task.id}/edit`} >
         <button>Edit</button>
